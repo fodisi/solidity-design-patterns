f9602fbb0f2a940d4961bb83f433dc03d8ba53d9
fodisi <francis2301@gmail.com>
2019-12-30 17:19:03 -0500

    Add new tests and passes initial value to constructor

--- upgradability/test/MySmartContract-v2.test.js
+++ upgradability/test/MySmartContract-v2.test.js
@@ -1,20 +1,37 @@
 const {expect} = require("chai");
+const {expectRevert} = require("openzeppelin-test-helpers");
+
 
 const MySmartContract = artifacts.require("MySmartContract");
 
 // This tests version 2 of MySmartContract.
 contract("MySmartContract", function([_, deployer ]) {
 
-  it("increments counter", async function() {
-    this.contract = await MySmartContract.new(100, {from: deployer});
-    
+  it("sets counter value on deployment", async function() {
+    this.contract = await MySmartContract.new(10, {from: deployer});
+
+    let counter = await this.contract.counter.call();
+    expect(counter.toNumber()).to.equal(10);
+
+  });
+
+
+  it("increments counter by one", async function() {
+    this.contract = await MySmartContract.new(0, {from: deployer});
+
     await this.contract.incrementCounter();
     let counter = await this.contract.counter.call();
-    expect(counter.toNumber()).to.equal(101);
+    expect(counter.toNumber()).to.equal(1);
 
     await this.contract.incrementCounter();
     counter = await this.contract.counter.call();
-    expect(counter.toNumber()).to.equal(102);
+    expect(counter.toNumber()).to.equal(2);
+  });
+
+  it("reverts when stopped", async function() {
+    this.contract = await MySmartContract.new(0, {from: deployer});
+    await this.contract.toggleContractStopped({from: deployer});
+    await expectRevert(this.contract.incrementCounter(), "Contract is stopped.");
   });
 
 });
