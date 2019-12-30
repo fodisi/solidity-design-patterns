3de2b34a67b21e2aff37770581def444d382c9ae
fodisi <francis2301@gmail.com>
2019-12-30 17:15:58 -0500

    Implements circuit breaker and restriction mechanisms

--- upgradability/contracts/MySmartContract-v2.sol
+++ upgradability/contracts/MySmartContract-v2.sol
@@ -2,12 +2,43 @@ pragma solidity ^0.5.0;
 
 contract MySmartContract {
   uint32 public counter;
+  bool private stopped = false;
+  address private owner;
 
-  constructor(uint32 _counter) public {
-    counter = _counter;
+  /**
+  @dev Checks if the contract is not stopped; reverts if it is.
+  */
+  modifier isNotStopped {
+    require(!stopped, 'Contract is stopped.');
+    _;
   }
 
-  function incrementCounter() public {
-    counter++;
+  /**
+  @dev Enforces the caller to be the contract's owner.
+  */
+  modifier isOwner {
+    require(msg.sender == owner, 'Sender is not owner.');
+    _;
+  }
+
+  constructor(uint32 _counter) public {
+    counter = _counter; // Allows setting counter's initial value on deployment.
+    // Sets the contract's owner as the address that deployed the contract.
+    owner = msg.sender;
+  }
+
+  /**
+  @notice Increments the contract's counter if contract is active.
+  @dev It will revert is the contract is stopped. See modifier "isNotStopped"
+   */
+  function incrementCounter() isNotStopped public {
+    counter++; // Fixes bug introduced in version 1.
+  }
+
+  /**
+  @dev Stops / Unstops the contract.
+   */
+  function toggleContractStopped() isOwner public {
+      stopped = !stopped;
   }
 }
